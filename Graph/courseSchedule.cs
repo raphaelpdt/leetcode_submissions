a9b4ece34748d3f6ba5215fb048e10b243885068
Raphael <raphaeldetagle@gmail.com>
2021-06-09 17:08:39 -0700

    Modified courseSched

--- Graph/courseSchedule.cs
+++ Graph/courseSchedule.cs
@@ -1,5 +1,7 @@
-public class Solution {
-    public bool CanFinish(int numCourses, int[][] prerequisites) {
+public class Solution
+{
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
         // build dict representation of graph
         Dictionary<int, List<int>> clone = new Dictionary<int, List<int>>();
         foreach (var course in prerequisites)
@@ -8,7 +10,7 @@ public class Solution {
             int prerequisite = course[1];
             if (!clone.ContainsKey(curr))
             {
-                List<int> prereqs = new List<int>{prerequisite};
+                List<int> prereqs = new List<int> { prerequisite };
                 clone.Add(curr, prereqs);
             }
             else
@@ -16,32 +18,32 @@ public class Solution {
                 clone[curr].Add(prerequisite);
             }
         }
-        
+
         int[] visited = new int[numCourses]; // use for cycle detection
         for (int i = 0; i < numCourses; i++)
         {
             if (!HasCycle(visited, clone, i))
-                return false;           
+                return false;
         }
-        
+
         return true;
     }
-    
-    public static bool HasCycle (int[] visited, Dictionary<int, List<int>> graph, int node)
+
+    public static bool HasCycle(int[] visited, Dictionary<int, List<int>> graph, int node)
     {
         if (visited[node] == 1) return true;
         if (visited[node] == -1) return false;
-        
+
         visited[node] = -1;
         if (graph.ContainsKey(node))
         {
             foreach (var course in graph[node])
             {
                 if (!HasCycle(visited, graph, course))
-                    return false;                
+                    return false;
             }
         }
-        
+
         // mark node as being part of a cycle
         visited[node] = 1;
         return true;
