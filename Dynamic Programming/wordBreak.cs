85ed492d93f3f2b0c6b705a337fd75a3b0fd75ac
Raphael <raphaeldetagle@gmail.com>
2021-08-17 15:21:41 -0700

    Edited wordBreak.cs

--- Dynamic Programming/wordBreak.cs
+++ Dynamic Programming/wordBreak.cs
@@ -1,22 +1,24 @@
-public class Solution {
-    public bool WordBreak(string s, IList<string> wordDict) {
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
         bool[] dp = new bool[s.Length + 1];
         dp[0] = true;
-        
+
         for (int i = 0; i < s.Length; i++)
         {
             if (dp[i])
             {
                 foreach (var word in wordDict)
                 {
-                    if ((word.Length + i -1) < s.Length && String.Equals(s.Substring(i, word.Length), word))
+                    if ((word.Length + i - 1) < s.Length && String.Equals(s.Substring(i, word.Length), word))
                     {
                         dp[word.Length + i] = true;
                     }
                 }
             }
         }
-        
+
         return dp[s.Length];
     }
 }
